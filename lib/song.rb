615bb2441aa6eaacd7ffe238803f72c62f7336f6
Dan Hwang <danhwangfkpc@gmail.com>
2019-04-17 02:55:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,8 @@ require 'pry'
 class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  include Paramable::Instance Methods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
